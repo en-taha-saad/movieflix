cdd7ca8b4ba96c1c154aceec00e1fde3c6ea5297
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-09 00:33:48 +0300

    initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# movieflix
+
+app about movies
---
+++ android/app/src/main/kotlin/com/movie/movieflix/movieflix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.movie.movieflix.movieflix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
