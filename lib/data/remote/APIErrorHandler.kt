3d6012f934139c7620088554a70727b76c07e3ef
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-12 11:55:57 +0300

    Revert "kotlin files to convert into flutter"
    This reverts commit b7ddce8b39f2063e38806ab9ac8d1e9c8d232a2a.

--- lib/data/remote/APIErrorHandler.kt
+++
@@ -1,8 +0,0 @@
-package com.red_velvet.flix.data.remote
-
-import com.red_velvet.flix.domain.utils.FlixException
-import okhttp3.ResponseBody
-
-interface APIErrorHandler {
-    fun getFlixException(errorBody: ResponseBody?): FlixException
-}
--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -18,7 +18,6 @@ extension GetItInjectableX on _i1.GetIt {
     String? environment,
     _i2.EnvironmentFilter? environmentFilter,
   }) {
-    // ignore: unused_local_variable
     final gh = _i2.GetItHelper(
       this,
       environment,
