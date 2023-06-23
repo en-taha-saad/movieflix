a8af2ffefd54c21d8fde70e992279df660786d53
Parabeac-Cloud <admin@parabeac.com>
2023-06-23 14:21:34 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ android/app/src/main/kotlin/com/example/movieflex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movieflex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/movieflex_theme.g.dart
@@ -0,0 +1,19 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:movieflex/theme/movieflex_colors.g.dart';
+
+class MovieflexTheme {
+
+static const ColorScheme light = ColorScheme.light(onPrimary: MovieflexColors.lightOnPrimary,primary: MovieflexColors.lightPrimary,primaryContainer: MovieflexColors.lightPrimaryContainer,secondary: MovieflexColors.lightSecondary,onSecondary: MovieflexColors.lightOnSecondary,onPrimaryContainer: MovieflexColors.lightOnPrimaryContainer,secondaryContainer: MovieflexColors.lightSecondaryContainer,onSecondaryContainer: MovieflexColors.lightOnSecondaryContainer,shadow: MovieflexColors.lightShadow);
+
+static  ThemeData themeDataLight = ThemeData( colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: MovieflexColors.darkPrimary,onPrimary: MovieflexColors.darkOnPrimary,primaryContainer: MovieflexColors.darkPrimaryContainer,onPrimaryContainer: MovieflexColors.darkOnPrimaryContainer,secondary: MovieflexColors.darkSecondary,onSecondary: MovieflexColors.darkOnSecondary,secondaryContainer: MovieflexColors.darkSecondaryContainer,onSecondaryContainer: MovieflexColors.darkOnSecondaryContainer,shadow: MovieflexColors.darkShadow);
+
+static  ThemeData themeDataDark = ThemeData( colorScheme: dark,);
+}
